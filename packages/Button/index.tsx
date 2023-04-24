a4720dd78de303e13681610adb4c20d89b1a558d
tangjiahui <tjhaaa@163.com>
2023-04-24 23:31:32 +0800

    feat(button): 新增按钮组件

--- packages/Button/index.tsx
+++ packages/Button/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
-import Styles from './index.less';
+import React, { MouseEventHandler } from "react";
+import styles from "./index.less";
+
+export type ButtonType = "primary" | 'dashed' | 'default' | 'text' | 'dotted';
+export type ButtonSize = "large" | "middle" | "small";
 
 export interface ButtonProps {
-  type: 'primary' | 'default' | 'dashed';
+  // 按钮类型
+  type?: ButtonType;
+  // 是否禁用
+  disabled?: boolean;
+  // 块按钮
+  block?: boolean;
+  // 危险按钮
+  danger?: boolean;
+  // todo: 按钮添加'加载中'效果
+  // // 是否加载中
+  // loading?: boolean;
+  // 按钮大小
+  size?: ButtonSize;
+  // 内联样式
+  style?: React.CSSProperties;
+  // 点击事件回调
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  // 子元素
+  children?: any;
 }
 
-export default function () {
-  return <button className={Styles.my_btn}>按钮</button>;
+/**
+ * 按钮
+ *
+ * At 2023/04/24
+ * By TangJiaHui
+ */
+export default function Button (props: ButtonProps) {
+  const classes = [
+    styles['btn'],
+    styles[`btn-${props?.type}`],
+    styles[`btn-${props?.size}`],
+    props?.danger && styles['btn-danger'],
+    props?.block && styles['btn-block']
+  ].filter(Boolean).join(' ')
+
+  return <button
+    className={classes}
+    disabled={props?.disabled}
+    onClick={props?.onClick}
+    style={props?.style}
+  >
+    {props?.children}
+  </button>;
+}
+
+Button.defaultProps = {
+  type: 'default',
+  block: false,
+  disabled: false,
+  danger: false,
+  // loading: false,
+  size: 'middle'
 }
--- scripts/share/getPackages.js
+++ scripts/share/getPackages.js
@@ -10,13 +10,18 @@ const { root } = require('../index');
 /**
  * get directory name list under 'directoryPath'
  */
-function getPackageNames(directoryPath) {
+function getPackageNames(directoryPath, matchedRegExp) {
   if (!isExist(directoryPath)) return [];
   if (!isDirectory(directoryPath)) return [];
 
   const fileNameList = fs.readdirSync(directoryPath);
   return fileNameList.filter((fileName) => {
-    return isDirectory(path.resolve(directoryPath, fileName));
+    let matched = true;
+    if (matchedRegExp) {
+      matched = matchedRegExp.test(fileName)
+    }
+    return matched &&
+      isDirectory(path.resolve(directoryPath, fileName));
   });
 }
 
@@ -25,7 +30,8 @@ function getPackageNames(directoryPath) {
  * get directory name-absPath mapping under 'directoryPath'
  */
 function getPackageNamePathMap(directoryPath) {
-  return getPackageNames(directoryPath).reduce((namePathMap, name) => {
+  const matchedRegExp = /^[a-zA-Z](\w)*/
+  return getPackageNames(directoryPath, matchedRegExp).reduce((namePathMap, name) => {
     namePathMap[name] = root(directoryPath, name);
     return namePathMap;
   }, {});
