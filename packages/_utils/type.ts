5040ed3592d8ecb4738065ca25caaa75f67a827b
tangjiahui <tjhaaa@163.com>
2023-06-09 10:40:57 +0800

    build(.): delete useless files.(like rollup)

--- packages/_utils/type.ts
+++ packages/_utils/type.ts
@@ -1,10 +1,6 @@
-type Object = {
-  [k: string]: any;
-};
-
 export const isString = (o: any): o is string => typeof o === 'string';
 export const isNumber = (o: any): o is number => typeof o === 'number';
 export const isFunction = (o: any): o is Function => o instanceof Function;
 export const isArray = (o: any): o is any[] => Array.isArray(o);
-export const isObject = (o: any): o is Object => !!(typeof o === 'object' && o && !isArray(o));
+export const isObject = (o: any): boolean => !!(typeof o === 'object' && o && !isArray(o));
 export const isBoolean = (o: any): o is boolean => typeof o === 'boolean';
