5d354221366df81a1c01b2ac9a7ba25884359221
tangjiahui <tjhaaa@163.com>
2024-04-24 10:19:41 +0800

    refactor(tolltip): rewrite tooltip

--- packages/ToolTip/tooltip.tsx
+++ packages/ToolTip/tooltip.tsx
@@ -4,30 +4,51 @@
  * @author tangjiahui
  * @date 2024/02/02
  */
-import React, { DOMAttributes } from 'react';
+import React, { DOMAttributes, ForwardedRef, RefAttributes } from 'react';
 import { useMountDom } from '@/_hooks';
 import { CaretDownOutlined } from '@ant-design/icons';
+import classNames from 'classnames';
+import { usePrefix } from '@/ConfigProvider/ConfigProvider';
+import { omit } from '@/_utils/object';
+import './tooltip.less';
 
-export type ToolTipProps = {
+export type BaseToolTipProps = {
   /**
    * @description 标题
    */
   title?: React.ReactNode;
   /**
-   * @description 浮层样式
+   * style
    */
   style?: React.CSSProperties;
+  /**
+   * className
+   */
+  className?: string;
   /**
    * @description 包裹子元素
    */
   children?: React.ReactElement | string;
 };
 
-export default function ToolTip(props: ToolTipProps) {
+export type BaseToolTipKeys = keyof BaseToolTipProps;
+export type ToolTipProps = BaseToolTipProps &
+  DOMAttributes<HTMLDivElement> &
+  RefAttributes<HTMLDivElement>;
+
+const privateKeys: BaseToolTipKeys[] = ['children', 'title', 'style', 'className'];
+export type ToolTipFC = React.ForwardRefExoticComponent<ToolTipProps>;
+const ToolTip: ToolTipFC = React.forwardRef(function ToolTip(
+  props: ToolTipProps,
+  ref: ForwardedRef<HTMLDivElement>,
+) {
   if (!props?.children) return;
   if (Array.isArray(props?.children)) {
     throw new Error('children must be a single react element.');
   }
+
+  const prefix = usePrefix('tooltip');
+  const originProps: DOMAttributes<HTMLDivElement> = omit(props, privateKeys);
 
   const children: React.ReactElement =
     typeof props?.children === 'string' ? <span>{props?.children}</span> : props?.children;
@@ -49,10 +70,13 @@ export default function ToolTip(props: ToolTipProps) {
       // TODO: rewrite style.
       return (
         <div
+          {...originProps}
+          className={classNames(props?.className, prefix)}
+          ref={ref}
           style={{
-            position: 'fixed',
             top: domRect.top,
             left: domRect.left + domRect.width / 2,
+            ...props?.style,
           }}
         >
           <div
@@ -112,4 +136,6 @@ export default function ToolTip(props: ToolTipProps) {
       children?.props?.onMouseLeave?.(e);
     },
   });
-}
+});
+
+export default ToolTip;
