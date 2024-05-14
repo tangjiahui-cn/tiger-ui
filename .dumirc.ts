99be9268c731c4a4f9ade40af96ea98a43a21cfa
tangjiahui <tjhaaa@163.com>
2024-05-14 21:56:50 +0800

    ci(.): update ci

--- .dumirc.ts
+++ .dumirc.ts
@@ -8,6 +8,7 @@ const { deploy } = process.env;
 const publicPath = deploy === 'github' ? `/${NAME}/` : '/';
 
 export default defineConfig({
+  base: publicPath,
   title: 'tiger-ui',
   outputPath: 'docs-dist',
   themeConfig: {
