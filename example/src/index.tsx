6c87ca8b7b0bde3aa0d59bcd98d7c0304583a8de
tangjiahui <tjhaaa@163.com>
2024-06-06 10:56:39 +0800

    fix(windowlistener): delete windowListener model

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,7 +5,6 @@ import '../../packages/_theme/default.less';
 import { useForceUpdate } from '@/_hooks';
 import Button, { ButtonSize, ButtonType } from '@/Button/button';
 import { Switch } from '@/index';
-import { windowListener } from '@/_model';
 
 const size: ButtonSize[] = ['large', 'middle', 'small'];
 const types: ButtonType[] = ['primary', 'default', 'dashed', 'dotted', 'text'];
@@ -27,12 +26,6 @@ function App() {
     function resize2() {
       console.log('222');
     }
-    windowListener.addEventListener('resize', resize, {
-      once: true,
-    });
-    windowListener.addEventListener('resize', resize2, {
-      once: true,
-    });
   }, []);
 
   return (
--- packages/_model/index.ts
+++ packages/_model/index.ts
@@ -1,4 +1,3 @@
 export * from './BaseEvent';
 export * from './GlobalListener';
 export * from './GlobalScroll';
-export * from './windowListener';
