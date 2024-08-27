ffc3ea4354e9aa7d64077907c74d00038630b3d3
tangjiahui <tjhaaa@163.com>
2024-08-27 10:39:23 +0800

    feat(useforceupdate): rewrite useForceUpdate hook

--- packages/_hooks/useForceUpdate.ts
+++ packages/_hooks/useForceUpdate.ts
@@ -5,9 +5,8 @@
  * @date 2024/5/27
  * @description update current component once.
  */
-import { useCallback, useState } from 'react';
+import { useReducer } from 'react';
 
 export function useForceUpdate(): () => void {
-  const [_, setV] = useState<boolean>(false);
-  return useCallback(() => setV((v: boolean) => !v), []);
+  return useReducer((v) => !v, false)[1];
 }
