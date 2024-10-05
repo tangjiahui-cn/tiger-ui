7c3e1d227662cdb573aea4775bd5b051df1f3364
tangjiahui <tjhaaa@163.com>
2024-10-05 17:31:23 +0800

    ci(.): fix ts error 2742 for win platform

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 export default function () {
   useLogRenderTime();
 
+  const [loading, setLoading] = useState(false);
   const [visible, setVisible] = useState(false);
   const [placement, setPlacement] = useState<any>('top');
 
@@ -25,7 +26,18 @@ export default function () {
       }}
       direction={'vertical'}
     >
-      <Button type={'primary'}>测试按钮</Button>
+      <Button
+        type={'primary'}
+        loading={loading}
+        onClick={() => {
+          setLoading(true);
+          setTimeout(() => {
+            setLoading(false);
+          }, 500);
+        }}
+      >
+        测试按钮
+      </Button>
       <Button onClick={() => changePrimary('red')}>红色</Button>
       <Button onClick={() => changePrimary('blue')}>蓝色</Button>
       <Space>
